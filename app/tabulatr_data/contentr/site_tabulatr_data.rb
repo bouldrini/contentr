5d97ff21eae4dce622af74b3f492a8fb1435897f
RichardWieditz <richardwieditz@gmail.com>
2013-12-20 13:59:22 +0100

    font-awesome-update

--- app/tabulatr_data/contentr/site_tabulatr_data.rb
+++ app/tabulatr_data/contentr/site_tabulatr_data.rb
@@ -4,7 +4,7 @@ class Contentr::SiteTabulatrData < Tabulatr::Data
   #  "#" # edit_link edit_backend_tariff_path(rec)
   # end
   # column :destroy, table_column_options: {header: '', sortable: false,  filter: false, width: '30px', classes: 'hidden-phone hidden-tablet'} do |rec|
-  #   link_to "<div class='btn btn-danger'><i class='icon icon-trash'></i></div>".html_safe, "", method: :delete, id: rec.id, data: {delete: true, name: "der Tarif '#{rec.title}'"}
+  #   link_to "<div class='btn btn-danger'><i class='fa fa-trash'></i></div>".html_safe, "", method: :delete, id: rec.id, data: {delete: true, name: "der Tarif '#{rec.title}'"}
   # end
   column :name
 end
